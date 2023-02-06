af412b91a3c154cf3d102a6bdaaed6c3a6b6f648
Tuan Anh Nguyen <anhnt20101995@gmail.com>
2023-02-06 15:50:39 +0700

    add blog category

--- the_so_so_blog/blog_posts/models.py
+++ the_so_so_blog/blog_posts/models.py
@@ -6,11 +6,24 @@ from the_so_so_blog.users.models import User
 
 
 class BlogPost(models.Model):
+    # class BlogCategories(models.TextChoices):
+    #     PROG = "PROG", "Programming"
+    #     LIFE = "LIFE", "Life Stories"
+    #     WRITTEN = "WRITTEN", "Written Stories"
+    #     MISC = "MISC", "Miscellaneous"
+
+    BLOG_CATEGORIES = [
+        ("PROG", "Programming"),
+        ("LIFE", "Life Stories"),
+        ("WRITTEN", "Written Stories"),
+        ("MISC", "Miscellaneous"),
+    ]
     title = models.CharField(blank=False, null=False, max_length=120)
     content = models.TextField(blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(blank=False, null=False, choices=BLOG_CATEGORIES, default="LIFE", max_length=120)
 
     def __str__(self):
         return self.title
