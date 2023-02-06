8b73de89f4d17a24965181d569d3ead4ba7699fd
Tuan Anh Nguyen <anhnt20101995@gmail.com>
2023-02-06 16:01:13 +0700

    Add blog post permission to limit non-author actions

--- the_so_so_blog/blog_posts/permissions.py
+++ the_so_so_blog/blog_posts/permissions.py
@@ -5,6 +5,7 @@ from rest_framework.request import Request
 from rest_framework.views import APIView
 from ..utils.permissions import is_own_user
 
-class IsSameUser(BasePermission):
+
+class IsAuthor(BasePermission):
     def has_object_permission(self, request: Request, view: APIView, obj: Any) -> bool:
         return is_own_user(request, obj)
